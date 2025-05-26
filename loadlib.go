13e2207594095ff14a2ccf62dc4039402efdb0eb
kleeedolinux <nicolasketion@gmail.com>
2025-05-26 19:30:22 -0300

    v1.0.0

--- loadlib.go
+++ loadlib.go
@@ -14,35 +14,47 @@ var loLoaders = []LGFunction{loLoaderPreload, loLoaderLua}
 func loGetPath(env string, defpath string) string {
 	path := os.Getenv(env)
 	if len(path) == 0 {
-		path = defpath
+		return defpath
 	}
+
+	if !strings.Contains(path, ";;") {
+		return path
+	}
+
+	if os.PathSeparator == '/' {
+		return strings.Replace(path, ";;", ";"+defpath+";", -1)
+	}
+
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		panic(err)
+	}
+
 	path = strings.Replace(path, ";;", ";"+defpath+";", -1)
-	if os.PathSeparator != '/' {
-		dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-		if err != nil {
-			panic(err)
-		}
-		path = strings.Replace(path, "!", dir, -1)
-	}
-	return path
+	return strings.Replace(path, "!", dir, -1)
 }
 
 func loFindFile(L *LState, name, pname string) (string, string) {
 	name = strings.Replace(name, ".", string(os.PathSeparator), -1)
+
 	lv := L.GetField(L.GetField(L.Get(EnvironIndex), "package"), pname)
 	path, ok := lv.(LString)
 	if !ok {
 		L.RaiseError("package.%s must be a string", pname)
 	}
-	messages := []string{}
-	for _, pattern := range strings.Split(string(path), ";") {
+
+	var messages []string
+	patterns := strings.Split(string(path), ";")
+
+	for _, pattern := range patterns {
 		luapath := strings.Replace(pattern, "?", name, -1)
-		if _, err := os.Stat(luapath); err == nil {
+		_, err := os.Stat(luapath)
+		if err == nil {
 			return luapath, ""
-		} else {
-			messages = append(messages, err.Error())
 		}
+		messages = append(messages, err.Error())
 	}
+
 	return "", strings.Join(messages, "\n\t")
 }
 
@@ -55,6 +67,7 @@ func OpenPackage(L *LState) int {
 	for i, loader := range loLoaders {
 		L.RawSetInt(loaders, i+1, L.NewFunction(loader))
 	}
+
 	L.SetField(packagemod, "loaders", loaders)
 	L.SetField(L.Get(RegistryIndex), "_LOADERS", loaders)
 
@@ -65,8 +78,13 @@ func OpenPackage(L *LState) int {
 	L.SetField(packagemod, "path", LString(loGetPath(LuaPath, LuaPathDefault)))
 	L.SetField(packagemod, "cpath", emptyLString)
 
-	L.SetField(packagemod, "config", LString(LuaDirSep+"\n"+LuaPathSep+
-		"\n"+LuaPathMark+"\n"+LuaExecDir+"\n"+LuaIgMark+"\n"))
+	config := fmt.Sprintf("%s\n%s\n%s\n%s\n%s\n",
+		LuaDirSep,
+		LuaPathSep,
+		LuaPathMark,
+		LuaExecDir,
+		LuaIgMark)
+	L.SetField(packagemod, "config", LString(config))
 
 	L.Push(packagemod)
 	return 1
@@ -79,30 +97,36 @@ var loFuncs = map[string]LGFunction{
 
 func loLoaderPreload(L *LState) int {
 	name := L.CheckString(1)
+
 	preload := L.GetField(L.GetField(L.Get(EnvironIndex), "package"), "preload")
 	if _, ok := preload.(*LTable); !ok {
 		L.RaiseError("package.preload must be a table")
 	}
+
 	lv := L.GetField(preload, name)
 	if lv == LNil {
 		L.Push(LString(fmt.Sprintf("no field package.preload['%s']", name)))
 		return 1
 	}
+
 	L.Push(lv)
 	return 1
 }
 
 func loLoaderLua(L *LState) int {
 	name := L.CheckString(1)
+
 	path, msg := loFindFile(L, name, "path")
 	if len(path) == 0 {
 		L.Push(LString(msg))
 		return 1
 	}
-	fn, err1 := L.LoadFile(path)
-	if err1 != nil {
-		L.RaiseError(err1.Error())
+
+	fn, err := L.LoadFile(path)
+	if err != nil {
+		L.RaiseError(err.Error())
 	}
+
 	L.Push(fn)
 	return 1
 }
@@ -114,11 +138,13 @@ func loLoadLib(L *LState) int {
 
 func loSeeAll(L *LState) int {
 	mod := L.CheckTable(1)
+
 	mt := L.GetMetatable(mod)
 	if mt == LNil {
 		mt = L.CreateTable(0, 1)
 		L.SetMetatable(mod, mt)
 	}
+
 	L.SetField(mt, "__index", L.Get(GlobalsIndex))
 	return 0
 }
